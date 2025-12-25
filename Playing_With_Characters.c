bf247da6c9185b3d78c755375abd4f5c4f0d1103
Lakshmi937 <satyayarramneedi44@gmail.com>
2025-12-25 17:38:20 +0530

    Create Playing_With_Characters.c

---
+++ Playing_With_Characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+           char ch;
+           char s[99];
+           char sen[100];
+           scanf("%c\n",&ch);
+           scanf("%s\n",s);
+           scanf("%[^\n]s",sen);
+           printf("%c\n",ch);
+           printf("%s\n",s);
+           printf("%s",sen);
+     return 0;
+}
