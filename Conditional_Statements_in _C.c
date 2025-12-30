86560186cb2f2c37d9cf8fe698cfb91ddea1a777
Lakshmi937 <satyayarramneedi44@gmail.com>
2025-12-30 23:01:06 +0530

    Create Conditional_Statements_in _C.c

---
+++ Conditional_Statements_in _C.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main ()
+{
+    int k;
+    scanf("%d",&k);
+    if(k==1){
+        printf("one\n");
+    }
+    else if(k==2){
+        printf("two\n");
+    }
+    else if(k==3){
+        printf("three\n");
+    }
+    else if (k==4){
+        printf("four\n");
+    }
+    else if (k==5){
+        printf("five\n");
+    }
+    else if (k==6){
+        printf("six\n");
+    }
+    else if(k==7){
+        printf("seven\n");
+    }
+    else if(k==8){
+        printf("eight\n");
+    }
+    else if(k==9){
+        printf("nine");
+    }
+    else if(k>9){
+        printf("Greater than 9\n");
+    }
+
+    return 0;
+}
