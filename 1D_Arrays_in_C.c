5e39f31a63bca21585e2eaf65ae3dc075997284c
Lakshmi937 <satyayarramneedi44@gmail.com>
2026-01-02 21:08:36 +0530

    Create 1D_Arrays_in_C.c

---
+++ 1D_Arrays_in_C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int *arr=(int*)malloc(n*sizeof(int));
+    int i,sum=0;
+    for(i=0;i<n;i++){
+        scanf("%d",arr+i);
+        sum=sum+*(arr+i);
+    }    
+    printf("%d\n",sum);
+    return 0;
+}
